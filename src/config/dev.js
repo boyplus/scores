f59c604ac306a54e531e8faeeef699d2af6025c6
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2020-09-03 15:37:33 +0700

    [UPDATE] gitignore

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:
-        'mongodb+srv://boyplus:Mv@TRynQ6D6NcQu@cluster0.nvc94.mongodb.net/scores?retryWrites=true&w=majority',
-    jwtSecret: 'kimseokjinwithluvandhappybirthdayjungkook',
-};
